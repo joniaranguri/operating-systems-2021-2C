d717948048a303c1acb62a3f4d6523f74808c279
0xskdzz <55303464+sm4rtk1dz@users.noreply.github.com>
2021-09-04 17:23:11 -0300

    Update ej-1.sh

--- APL 1/Ej-1/ej-1.sh
+++ APL 1/Ej-1/ej-1.sh
@@ -25,27 +25,55 @@
 
 ################ MAIN ####################
 
-funcA() {
-echo "Error. La sintaxis del script es la siguiente:"
-echo "......................: $0 directorio 5" # COMPLETAR
+#!/bin/bash
+
+funcA() { 
+    echo "Error. La sintaxis del script es la siguiente:"
+
+    # echo "......................: $0 directorio 5" (Punto .3)
+    echo "SCRIPT_NAME DIRECTORIO CANTIDAD_DE_CANDIDATOS_A_MOSTRAR: $0 directorio 5"
 }
+
 funcB() {
-echo "Error. $1 ....................." # COMPLETAR
+    # echo "Error. $1 ....................." # COMPLETAR (Punto .3)
+    echo "Error. $1 No es un directorio"
 }
-funcC() {
-if [[ ! -d $2 ]]; then
-funcB
-fi
+
+funcC() {  # (Punto .2) Realiza verificacion de parametro(s)
+    if [[ ! -d $2 ]]; then # (Punto .2) Verifica si el primer parametro pasado al script es un directorio.
+        funcB
+    fi
 }
+
 funcC $# $1 $2 $3 $4 $5
-LIST=$(ls -d $1*/)
+
+# (Respuesta .1) El objetivo del script es mostrar una lista acotada de Rutas para un directorio dado.
+# El script utiliza 2 parametros el directorio absoluto, y un numero que indica la cantidad de candidatos a mostrar.
+
+# (Respuesta .4) ¿Qué nombre debería tener las funciones funcA, funcB, funcC?
+# funcA deberia llamarse sintaxisReminder
+# funcB deberia llamarse directoryError
+# funcC deberia llamarse parameterVerification
+
+# (Respuesta .5) ¿Agregaría alguna otra validación a los parámetros? ¿Existe algún error en el script?
+# Dado que solo se verifica el parametro $1 en funcC, si implementaria validaciones en todos los parametros, esto es, que el segundo parametro sea un numero.
+# El script tiene varios errores, a la funcC se le pasan parametros que no se utilizan, y a funcB no se le pasan parametros que si utiliza.
+# Ademas el script no funciona si el directorio no es expresado correctamente con la terminacion "/" la funcion funcA nunca es invocada.
+
+# (Respuesta .6) $# indica la cantidad de parametros.
+# (Respuesta .8) Se manejara el error invocando la funcC.
+
+LIST=$(ls -d $1*/) # (Punto .2) Busca todos los subdirectorios del directorio pasado como primer parametro.
 ITEMS=()
+
 for d in $LIST; do
-ITEM="`ls $d | wc -l`-$d"
-ITEMS+=($ITEM)
+    ITEM="`ls $d | wc -l`-$d" # (Punto .2) Prefija cada subdirectorio con el numero de letras de su PATH.
+    ITEMS+=($ITEM)
 done
-IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]}))
-CANDIDATES="${sorted[*]:0:$2}"
+
+IFS=$'\n' sorted=($(sort -rV -t '-' -k 1 <<<${ITEMS[*]})) # (Punto .2) Ordena el array de directorios por numero de letras de su PATH.
+CANDIDATES="${sorted[*]:0:$2}" # (Punto .2) Selecciona los primeros N candidatos, donde N es el segundo parametro del script.
 unset IFS
-echo "....................." # COMPLETAR
-printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})"
+
+echo "Rutas candidatos encontrados:"
+printf "%s\n" "$(cut -d '-' -f 2 <<<${CANDIDATES[*]})" # (Punto .2) Muestra por pantalla los directorios candidatos.

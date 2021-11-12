8318510c0d55d593e3e9635d51373eb0ef425be5
joniaranguri <arangurim@gmail.com>
2021-11-12 05:06:56 -0300

    Ej 4 listo

--- APL 3/Ej-4/cliente.cpp
+++ APL 3/Ej-4/cliente.cpp
@@ -18,24 +18,21 @@
 #include <sys/mman.h>
 
 using namespace std;
+sem_t *semaforo;
+int servidorPid;
 #define MEMORIA_RES "aranggueri-mem"
 #define MEMORIA_DEF "DFFDF-gemem"
 #define SEM_LEE_CLIENTE "sem-legfe-cliente"
 #define SEM_LEE_SERVIDOR "sem-legef-servidor"
-#define SEM_CLIENTE "sem-DSDFSDFfDDDem"
-void handlerSenial(int signal)
-{
-  //Nothing to do
-}
+#define SEM_CLIENTE "sem-ssscdsfsdlissesddnfftefbro"
+#define USER_SKIP_QUESTION "user_skip_question"
+#define MEMORIA_PID "user_sSDFSDFSDFkip_question"
 
 void mostrarAyuda()
 {
-  printf("\n Ejemplo de ejecucion: \n ./cliente\n");
-  cout << "Comandos validos" << endl;
-  cout << "       SUM {nombreChofer}" << endl;
-  cout << "       AVG_VALOR" << endl;
-  cout << "       LIST {nombreChofer}" << endl;
-  cout << "       MAX_GASOIL" << endl;
+  cout << endl
+       << " Ejemplo de ejecucion:" << endl
+       << "./cliente" << endl;
 }
 
 int validarParametros(int argc, char const *argv[])
@@ -54,99 +51,115 @@ int validarParametros(int argc, char const *argv[])
   }
   return 0;
 }
-
-void salir()
+void cerrarRecursos(int signal)
 {
+  sem_post(semaforo);
+  sem_close(semaforo);
+  sem_unlink(SEM_CLIENTE);
+  exit(0);
 }
 
-void cerrarAlmATAR(int signal)
+void reactToSigInt(int signal)
 {
-  sem_unlink(SEM_CLIENTE);
-  sem_unlink(SEM_LEE_CLIENTE);
-  sem_unlink(SEM_LEE_SERVIDOR);
-  return;
+  cout << "Partida cancelada" << endl;
+  if (servidorPid != 0)
+  {
+    kill(servidorPid, SIGUSR2);
+  }
+
+  cerrarRecursos(signal);
+}
+
+int isNumber(char const s[])
+{
+  for (int i = 0; s[i] != '\0'; i++)
+  {
+    if (isdigit(s[i]) == 0)
+      return 0;
+  }
+  return 1;
 }
 
 int main(int argc, char const *argv[])
 {
-  sem_t *semaforo = sem_open(SEM_CLIENTE, O_CREAT, 0600, 1);
+  semaforo = sem_open(SEM_CLIENTE, O_CREAT, 0600, 1);
 
   if (sem_trywait(semaforo) != 0)
   {
     cout << "No se puede ejecutar mas de una instancia del proceso" << endl;
-    //return 1;
-  }
-
-  /* shm_unlink(MEMORIA_DEF);
-    shm_unlink(MEMORIA_RES);
-    sem_unlink(SEM_LEE_SERVIDOR);
-    sem_unlink(SEM_LEE_CLIENTE);
-*/
-  if (validarParametros(argc, argv))
-  {
-    sem_post(semaforo);
-    sem_close(semaforo);
-    sem_unlink(SEM_CLIENTE);
     return 1;
   }
+  if (validarParametros(argc, argv))
+  {
+    cerrarRecursos(0);
+  }
 
-  signal(SIGINT, handlerSenial);
-  signal(SIGKILL, cerrarAlmATAR);
+  signal(SIGINT, reactToSigInt);
 
+  int idMemoriaPid = shm_open(MEMORIA_PID, O_CREAT | O_RDWR, 0600);
   int idMemoriaDefinicion = shm_open(MEMORIA_DEF, O_CREAT | O_RDWR, 0600);
   int idMemoriaRespuesta = shm_open(MEMORIA_RES, O_CREAT | O_RDWR, 0600);
   size_t tamRespuesta = sizeof(char) * 100;
   size_t tamDefinicion = sizeof(char) * 4096;
   ftruncate(idMemoriaDefinicion, sizeof(tamDefinicion));
   ftruncate(idMemoriaRespuesta, sizeof(tamRespuesta));
+  ftruncate(idMemoriaPid, sizeof(int));
   char *memoriaRes = (char *)mmap(NULL, tamRespuesta,
                                   PROT_READ | PROT_WRITE, MAP_SHARED, idMemoriaRespuesta, 0);
   char *memoriaDef = (char *)mmap(NULL, tamDefinicion,
                                   PROT_READ | PROT_WRITE, MAP_SHARED, idMemoriaDefinicion, 0);
+  int *memoriaPid = (int *)mmap(NULL, sizeof(int),
+                                PROT_WRITE, MAP_SHARED, idMemoriaPid, 0);
+  close(idMemoriaPid);
   close(idMemoriaDefinicion);
   close(idMemoriaRespuesta);
   sem_t *semLeeCliente = sem_open(SEM_LEE_CLIENTE, O_CREAT, 0600, 0);
   sem_t *semLeeServidor = sem_open(SEM_LEE_SERVIDOR, O_CREAT, 0600, 0);
 
-
-
-
-
-
-
-
-
-
   cout << "Bienvenido al autodefinido" << endl;
   cout << "Ingrese la cantidad de preguntas a realizar:" << endl;
 
-//Escribir cantidad de preguntas en memoria Respuesta
-  cin >> memoriaRes;
+  string consulta;
+  bool isValid = false;
+  while (!isValid)
+  {
+    getline(cin, consulta);
+    if (isNumber(&consulta[0]))
+    {
+      isValid = true;
+    }
+    else
+    {
+      cout << "No se ingresó un numero. Intente nuevamente:" << endl;
+    }
+  }
+
+  strcpy(memoriaRes, &(consulta)[0]);
+
   int sizeOfPartida = atoi(memoriaRes);
-//Escribir cantidad de preguntas en memoria Respuesta
+  servidorPid = *memoriaPid;
 
+  cout << "Generando definiciones aleatorias. Espere por favor..." << endl;
 
- sem_post(semLeeServidor); //Habilito a leer al Servidor
-
-
+  sem_post(semLeeServidor);
   for (int i = 0; i < sizeOfPartida; i++)
   {
-//Leer definicion desde memoria Definicion
-    sem_wait(semLeeCliente); 
+    sem_wait(semLeeCliente);
     cout << memoriaDef << endl;
-    cin >> memoriaRes;
-    sem_post(semLeeServidor); //toma la respuesta
-  }
-  sem_wait(semLeeCliente); //4 dame
-  cout << memoriaDef << endl;
 
-/*
-  sem_post(semaforo);
-  sem_close(semaforo);
-  sem_unlink(SEM_CLIENTE);
-  sem_unlink(SEM_LEE_CLIENTE);
-  sem_close(semLeeCliente);
-  sem_unlink(SEM_LEE_CLIENTE);
-*/
+    string respuesta;
+    getline(cin, respuesta);
+    if (respuesta == "")
+    {
+      respuesta = USER_SKIP_QUESTION;
+    }
+    strcpy(memoriaRes, &(respuesta)[0]);
+    sem_post(semLeeServidor);
+  }
+  sem_wait(semLeeCliente);
+  cout << endl
+       << memoriaDef << endl;
+  cout << "Fin de la partida" << endl;
+  cerrarRecursos(0);
   return 0;
 }
--- APL 3/Ej-4/servidor.cpp
+++ APL 3/Ej-4/servidor.cpp
@@ -21,36 +21,23 @@
 #include <vector>
 #include <mutex>
 #include <sstream>
-
-/*
-✓ Se ejecutará y quedará a la espera de que un cliente se conecte.
-✓ Al finalizar una partida, quedará a la espera de una nueva conexión.
-✓ Finalizará su ejecución al recibir la señal SIGUSR1, siempre y cuando no haya ninguna partida
-en curso.
-✓ Deberá configurar una nueva partida al recibir la conexión de un nuevo cliente.
-✓ Seleccionará N definiciones con sus respectivas palabras de manera aleatoria (al comenzar cada
-nueva partida), elegidas de un conjunto de definiciones y palabras posibles almacenadas en un
-archivo de texto plano.
-✓ Al recibir una respuesta ya sea correcta o incorrecta, se pasará a la siguiente definición.
-✓ Al recibir como respuesta una cadena vacía, se pasará a la siguiente definición.
-✓ Deberá contabilizar +1 punto por palabra correcta, -1 punto por palabra incorrecta y 0 por pasar
-la palabra.
-✓ Al completarse la lista de definiciones, enviará al cliente la siguiente información:
-✓ Puntuación final.
-✓ Lista de palabras incorrectas o no contestadas y su respectiva definición
-*/
+#include <time.h>
 
 using namespace std;
-char respuestaCliente[100];
 int parentPid;
 string respuesta;
 string archivo = "definiciones.txt";
-bool isPlaying = false;
+vector<int> elegidas;
+sem_t *semaforo;
+sem_t *semLeeCliente;
+sem_t *semLeeServidor;
 #define MEMORIA_RES "aranggueri-mem"
 #define MEMORIA_DEF "DFFDF-gemem"
 #define SEM_LEE_CLIENTE "sem-legfe-cliente"
 #define SEM_LEE_SERVIDOR "sem-legef-servidor"
-#define SEM_SERVIDOR "sem-DDDgDem"
+#define SEM_SERVIDOR "sems-fsefssfsdddfsrvidorbro"
+#define USER_SKIP_QUESTION "user_skip_question"
+#define MEMORIA_PID "user_sSDFSDFSDFkip_question"
 class AutoDef
 {
 public:
@@ -67,163 +54,48 @@ public:
 };
 vector<AutoDef> registros;
 
-void handlerSigInt(int signal)
+void cerrarRecursos(int signal)
 {
-    //Nothing to do
-}
-void handlerSigFin(int signal)
-{
-    if (!isPlaying)
-    {
-        exit(0);
-    }
-}
-/*
-void getSumForChofer(string nombre)
-{
-    int kmsSum = 0;
-    int gasoilSum = 0;
-    int valorSum = 0;
-    bool encontro = false;
-    for (int i = 0; i < registros.size(); i++)
-    {
-        Registro registro = registros.at(i);
-        if (strcmp(&(registro.chofer)[0], &nombre[0]) == 0)
-        {
-            kmsSum += registro.kms;
-            gasoilSum += registro.gasoil;
-            valorSum += registro.valor;
-            encontro = true;
-        }
-    }
 
-    std::stringstream ss;
-    if (encontro)
-    {
-        ss << "Km: " << kmsSum << endl
-           << "Gasoil: " << gasoilSum << "Litros" << endl
-           << "Valor: $" << valorSum << endl;
-    }
-    else
-    {
-        ss << "No se encontró al chofer de nombre " << nombre << endl;
-    }
-    respuesta = ss.str();
+    shm_unlink(MEMORIA_RES);
+    shm_unlink(MEMORIA_DEF);
+    shm_unlink(MEMORIA_PID);
+    sem_close(semLeeCliente);
+    sem_close(semLeeServidor);
+    sem_unlink(SEM_LEE_CLIENTE);
+    sem_unlink(SEM_LEE_SERVIDOR);
+    sem_post(semaforo);
+    sem_close(semaforo);
+    sem_unlink(SEM_SERVIDOR);
+    cout << "Apagando servidor" << endl;
+    exit(0);
 }
 
-void getListForChofer(string nombre)
+void clienteFinaliza(int signal)
 {
-    std::stringstream ss;
-    bool encontro = false;
-
-    for (int i = 0; i < registros.size(); i++)
-    {
-        Registro registro = registros.at(i);
-        if (strcmp(&(registro.chofer)[0], &nombre[0]) == 0)
-        {
-            string guion = "-";
-            string customRuta = registro.ruta.replace(registro.ruta.find(guion), guion.length(), " -> ");
-            ss << registro.id << " - " << customRuta << " ( " << registro.kms << " Km )"
-               << " - " << registro.gasoil << " Lt - $ " << registro.valor << endl;
-            encontro = true;
-        }
-    }
-    if (!encontro)
-    {
-        ss << "No se encontró al chofer de nombre " << nombre << endl;
-    }
-    respuesta = ss.str();
+    cout << "Partida cancelada" << endl;
+    cerrarRecursos(signal);
 }
 
-void maxGasoil()
-{
-    std::stringstream ss;
-
-    sort(registros.begin(), registros.end(), greater<Registro>());
-    Registro registro = registros.at(0);
-    ss << "ID: " << registro.id << endl
-       << "Chofer: " << registro.chofer << endl
-       << "Trayecto: " << registro.ruta << " ( " << registro.kms << " Km ) " << endl
-       << "Gasoil: " << registro.gasoil << "Lt" << endl;
-    respuesta = ss.str();
-}
-
-void avgValor()
-{
-    int total = 0;
-    for (int i = 0; i < registros.size(); i++)
-    {
-        total += registros.at(i).valor;
-    }
-    std::stringstream ss;
-    ss << "$ " << total / registros.size() << endl;
-    respuesta = ss.str();
-}
-
-int procesarConsulta()
-{
-    if (registros.size() < 1)
-    {
-        respuesta = "No hay registros en el archivo";
-    }
-    istringstream ss(respuestaCliente);
-    string comando;
-    getline(ss, comando, ' ');
-
-    if (strcmp(&comando[0], "SUM") == 0)
-    {
-        string nombre;
-        getline(ss, nombre, ' ');
-        getSumForChofer(nombre);
-    }
-    else if (strcmp(&comando[0], "MAX_GASOIL") == 0)
-    {
-        maxGasoil();
-    }
-    else if (strcmp(&comando[0], "AVG_VALOR") == 0)
-    {
-        avgValor();
-    }
-    else if (strcmp(&comando[0], "LIST") == 0)
-    {
-        string nombre;
-        getline(ss, nombre, ' ');
-        getListForChofer(nombre);
-    }
-    else
-    {
-        return 1;
-    }
-
-    return 0;
-}
-*/
 void mostrarAyuda()
 {
-    printf("\n Ejemplo de ejecucion: \n ./servidor 2021.viajes\n");
+    cout << "Ejemplo de ejecucion: \n ./servidor" << endl;
 }
 
 int validarParametros(int argc, char const *argv[])
 {
-    if (argc != 2 || argc > 2)
+    if (argc > 1)
     {
-        printf("\n Cantidad de parametros incorrecta, verifique la ayuda\n");
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-?") == 0)
+        {
+            mostrarAyuda();
+        }
+        else
+        {
+            printf("\n Cantidad de parametros incorrecta, verifique la ayuda\n");
+        }
         return 1;
     }
-
-    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-?") == 0)
-    {
-        mostrarAyuda();
-        return 1;
-    }
-    struct stat info;
-    if (stat(argv[1], &info) != 0)
-    {
-        printf("\nNo se se econtró el archivo %s\n", argv[2]);
-        return 1;
-    }
-    archivo = argv[1];
-
     return 0;
 }
 
@@ -251,95 +123,132 @@ int leerArchivo()
     }
     return 0;
 }
-void cerrarAlmATAR(int signal)
+
+void elegirAleatorias(int cant)
 {
-    sem_unlink(SEM_SERVIDOR);
-    sem_unlink(SEM_LEE_CLIENTE);
-    sem_unlink(SEM_LEE_SERVIDOR);
-    return;
+    elegidas.clear();
+    srand(time(NULL));
+
+    while (elegidas.size() < cant)
+    {
+        int randomNum = rand() % registros.size();
+        if (find(elegidas.begin(), elegidas.end(), randomNum) == elegidas.end())
+        {
+            elegidas.push_back(randomNum);
+        }
+    }
 }
 
 int main(int argc, char const *argv[])
 {
 
-    sem_t *semaforo = sem_open(SEM_SERVIDOR, O_CREAT, 0600, 1);
+    semaforo = sem_open(SEM_SERVIDOR, O_CREAT, 0600, 1);
 
     if (sem_trywait(semaforo) != 0)
     {
         cout << "No se puede ejecutar mas de una instancia del proceso" << endl;
-        // return 1;
-    }
-    /* if (validarParametros(argc, argv))
-    {
-        sem_post(semaforo);
-        sem_close(semaforo);
-        sem_unlink(SEM_SERVIDOR);
         return 1;
-    }*/
+    }
+
     signal(SIGINT, SIG_IGN);
-    signal(SIGUSR1, handlerSigFin);
-    signal(SIGKILL, cerrarAlmATAR);
+    signal(SIGUSR1, cerrarRecursos);
+    signal(SIGUSR2, clienteFinaliza);
     if (leerArchivo() != 0)
     {
         cout << "No se puede leer el archivo de definiciones" << endl;
-
-        return 0;
+        cerrarRecursos(0);
     }
-    cout << registros.size() << endl;
-
+    int idMemoriaPid = shm_open(MEMORIA_PID, O_CREAT | O_RDWR, 0600);
     int idMemoriaDefinicion = shm_open(MEMORIA_DEF, O_CREAT | O_RDWR, 0600);
     int idMemoriaRespuesta = shm_open(MEMORIA_RES, O_CREAT | O_RDWR, 0600);
     size_t tamRespuesta = sizeof(char) * 100;
     size_t tamDefinicion = sizeof(char) * 4096;
+    ftruncate(idMemoriaPid, sizeof(int));
     ftruncate(idMemoriaDefinicion, sizeof(tamDefinicion));
     ftruncate(idMemoriaRespuesta, sizeof(tamRespuesta));
     char *memoriaRes = (char *)mmap(NULL, tamRespuesta,
                                     PROT_READ | PROT_WRITE, MAP_SHARED, idMemoriaRespuesta, 0);
     char *memoriaDef = (char *)mmap(NULL, tamDefinicion,
                                     PROT_READ | PROT_WRITE, MAP_SHARED, idMemoriaDefinicion, 0);
+    int *memoriaPid = (int *)mmap(NULL, sizeof(int),
+                                  PROT_WRITE, MAP_SHARED, idMemoriaPid, 0);
+    close(idMemoriaPid);
     close(idMemoriaDefinicion);
     close(idMemoriaRespuesta);
-    sem_t *semLeeCliente = sem_open(SEM_LEE_CLIENTE, O_CREAT, 0600, 0);
-    sem_t *semLeeServidor = sem_open(SEM_LEE_SERVIDOR, O_CREAT, 0600, 0);
-
-
-
-
-
-
-
-
+    semLeeCliente = sem_open(SEM_LEE_CLIENTE, O_CREAT, 0600, 0);
+    semLeeServidor = sem_open(SEM_LEE_SERVIDOR, O_CREAT, 0600, 0);
+    *memoriaPid = getpid();
 
     while (true)
     {
-        sem_wait(semLeeServidor); // servidor: quiero leerMemoria
+        cout << "Esperando conexión..." << endl;
+        sem_wait(semLeeServidor);
 
-        //configurarPartida
-        int a[] = {4, 0, 2};
+        signal(SIGUSR1, SIG_IGN);
+        *memoriaPid = getpid();
+        cout << "Conectado!" << endl;
 
+        int puntajePartida = 0;
+        stringstream ssError;
+        stringstream ssSkip;
+        ssError << "Definiciones no acertadas : " << endl;
+        ssSkip << "Definiciones saltadas : " << endl;
         int sizeOfPartida = atoi(memoriaRes);
+        elegirAleatorias(sizeOfPartida);
+        int saltadas = 0;
+        int erradas = 0;
 
         for (int i = 0; i < sizeOfPartida; i++)
         {
-            strcpy(memoriaDef, &(registros.at(i).definition)[0]);
-
-            cout << memoriaDef << endl;
-            sem_post(semLeeCliente); 
-
+            AutoDef def = registros.at(elegidas[i]);
+            char *definition = &(def.definition)[0];
+            char *result = &(def.result)[0];
+            stringstream definitionFormal;
+            definitionFormal << definition << " ( " << strlen(result) << " letras ) ";
+            strcpy(memoriaDef, &(definitionFormal.str())[0]);
+            sem_post(semLeeCliente);
             sem_wait(semLeeServidor);
-            string respuesta = memoriaRes;
-           // cout << "La respuesta es" << respuesta << endl;
+            if (strcmp(&(memoriaRes)[0], result) == 0)
+            {
+                puntajePartida++;
+            }
+            else if (strcmp(&(memoriaRes)[0], USER_SKIP_QUESTION) == 0)
+            {
+                ssSkip << definition
+                       << "--> " << result << endl
+                       << endl;
+                saltadas++;
+            }
+            else
+            {
+                ssError << definition
+                        << "--> " << result << endl
+                        << endl;
+                puntajePartida--;
+                erradas++;
+            }
         }
-        strcpy(memoriaDef, "Resultado final: 10");
-        sem_post(semLeeCliente); //4 dame
+
+        stringstream resultadoFinal;
+
+        if (saltadas > 0)
+        {
+            resultadoFinal << ssSkip.str() << endl
+                           << endl;
+        }
+        if (erradas > 0)
+        {
+            resultadoFinal << ssError.str() << endl;
+        }
+
+        resultadoFinal << "#########################" << endl
+                       << " * Puntuación final: "
+                       << puntajePartida << " *" << endl
+                       << "#########################" << endl;
+        strcpy(memoriaDef, &(resultadoFinal.str())[0]);
+        sem_post(semLeeCliente);
         cout << "Termino la partida, reiniciando" << endl;
+        signal(SIGUSR1, cerrarRecursos);
     }
-    sem_post(semaforo);
-    sem_close(semaforo);
-    sem_unlink(SEM_SERVIDOR);
-    sem_close(semLeeCliente);
-    sem_unlink(SEM_LEE_CLIENTE);
-    sem_close(semLeeServidor);
-    sem_unlink(SEM_LEE_SERVIDOR);
     return 0;
 }
